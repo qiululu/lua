147e4f652c9881f64235cac54a2a83809c2940d0
qiululu <qiululu1116@sina.cn>
2015-05-20 05:54:17 +0800

    处理简写标签，默认数值，属性列表等功能

--- regluar.lua
+++ regluar.lua
@@ -5,34 +5,53 @@ richText.innerTb = innerTb
 
 local function drawText(text, font, size, color, x, y)
   local nSize = 20
-  if type(size) ~= "number" then
-    nSize = tonumber(size)
+  if size ~= nil then
+    if type(size) ~= "number" then
+      nSize = tonumber(size)
+    else
+      nSize = size
+    end
   end
   return display.newTTFLabel({text = text, font=font, size=nSize, color=cc.c3b(255,255,0), x=x, y=y, valign=cc.VERTICAL_TEXT_ALIGNMENT_CENTER})
 end
 
 local function getPairs(str, tb)
   local node = display.newNode()
-  for k, attr, v in string.gmatch(str,"<(%w+)%s?(.*)>(.-)</%1>") do
-      print("name:"..k)
-      print("text:"..v)
+  --处理简写的标签
+  local strFull = string.sub(str, '<(%w+)(%s?.*)/>', '<%1%2></%1>')
+
+  --析取标签名，参数列表，文本值
+  for k, attr, v in string.gmatch(strFull,'<(%w+)%s?(.*)>(.-)</%1>') do
+      print("name:"..k..'\n')
+      print("text:"..v..'\n')
     local tb = {}
     innerTb[k] = tb
-    tb.value = v
     
     local tbAtrr = {}
+    tb.value = v
     tb.attr = tbAtrr 
     
+    --创建标签参数列表
     if attr ~= nil then
-      for ak, av in string.gmatch(attr,"(%w+)=(%S*)") do
+      for ak, av in string.gmatch(attr,'(%w+)=(%S*)') do
         tbAtrr[ak] = av or nil
-        print(ak.."："..tbAtrr[ak])  
+
+        print(ak.."："..tbAtrr[ak]..'\n')  
       end
     end
     
-    if k=="font" then
+    if k == 'font' then
       local lab = drawText(v, tbAtrr.type, tbAtrr.size, tbAtrr.color, 200, 300)
       node:add(lab)
+    elseif k == 'br' then
+      --处理换行
+    elseif k == 'img' then
+      if tbAtrr.src then
+        -- 根据默认或手动添加的路径加载图片
+      end
+      if tbAtrr.width or tbAtrr.height then
+        -- 指定控件大小，适配尺寸
+      end
     end
     
     print('----------------\n')
@@ -43,7 +62,6 @@ end
 
 richText.parseXML = function(str)
   local ret = nil
-  print("inputString: "..str.."TYPE:"..type(str).."\n")
   if type(str) == "string" then
     ret = getPairs(str, innerTb)
   end
